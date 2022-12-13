9c32f44e3d211ca2ecb891ee01fcd4ff0435527d
GuillaumeK <art.book@hotmail.fr>
2022-12-13 16:40:12 +0100

    Exo 15 - Création des personnes

---
+++ Exercice 1/Exo15.php
@@ -0,0 +1,48 @@
+<h1>Exercice 15</h1>
+
+<p>Créer une classe Personne (nom, prénom et date de naissance).
+    Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
+    <br>
+    $p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
+    $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;
+</p>
+<br><br><br><br><br>
+
+<?php
+
+class Personne
+{
+    private $_nom;
+    private $_prenom;
+
+    private $_dtNaissance;
+
+    public function __construct($nom, $prenom, $dtNaissance)
+    {
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_dtNaissance = $dtNaissance;
+    }
+
+    public function calcAge($dtNaissance)
+    {
+        $dtAujourdhui = date("Y-m-d");
+        $diff = date_diff(date_create($dtNaissance), date_create($dtAujourdhui));
+        
+    }
+}
+
+// $dtNaissance = "19-02-1980";
+// $dtAujourdhui = date("Y-m-d");
+// $diff = date_diff(date_create($dtNaissance), date_create($dtAujourdhui));
+
+
+
+$personne1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$personne2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
+
+var_dump($personne1, $personne2);
+echo 'Michel DUPONT à ' . $diff->format('%y ans <br>');
+echo 'Alice DUCHEMIN à ' . $diff->format('%y ans');
+
+?>
