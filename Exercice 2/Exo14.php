50b5247cf776270a4e09ac28a33204afe9a6c5da
Zicka67 <art.book@hotmail.fr>
2022-12-15 09:59:46 +0100

    Début de l'exercice 14 - création de la classe

---
+++ Exercice 2/Exo14.php
@@ -0,0 +1,27 @@
+<p>Créer une classe Voiture possédant 2 propriétés(marque et modèle) ainsi qu’une classe VoitureElec qui hérite(extends) 
+    de la classe Voiture et qui a une propriété supplémentaire (autonomie).
+    Instancier une voiture «classique» et une voiture «électrique»ayant les caractéristiques suivantes: <br>
+    Peugeot 408: $v1 = new Voiture("Peugeot","408"); <br>
+    BMW i3150: $ve1 = new VoitureElec("BMW","I3",100);<br> <br>
+    Votre programme de test devra afficher les informations des 2 voitures de la façon suivante: <br>
+    echo $v1->getInfos()."<br/>"; <br>
+    echo $ve1->getInfos()."<br/>";<br>
+</p>
+<br>
+
+<br><br><br><br><br>
+
+
+
+<?php
+
+class Voiture 
+{
+    
+
+
+}
+
+
+$voiture1 = new Voiture ();
+$voiture2 = new Voiture ();
