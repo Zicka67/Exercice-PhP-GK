27cdd8379ff6409f90b099fc7a37664d7d40ec9c
Zicka67 <art.book@hotmail.fr>
2022-12-20 10:57:33 +0100

    Avancée sur l'exo

--- Exercice POO Banque/index.php
+++ Exercice POO Banque/index.php
@@ -14,14 +14,27 @@
 
 <?php
 
+// require "Compte.php";
+// require "Titulaire.php";
 
-require "Compte.php";
-require "Titulaire.php";
+spl_autoload_register(function ($class_name) {
+
+    require_once $class_name . '.php';
+
+});
+
+// Instancier un titulaire de compte
+$titulaire = new Titulaire("Musk", "Elon", 28-06-1971,"Pretoria");
+
+// Instancier un compte
+$compte1 = new Compte("Compte courant", 5000000, "Dollars", $titulaire);
+$compte2 = new Compte("Livret A", 1000000, "Dollars", $titulaire);
 
 
+echo $titulaire->afficherComptes;
 
-
-
+var_dump($compte1);
+var_dump($titulaire);
 
 
 
