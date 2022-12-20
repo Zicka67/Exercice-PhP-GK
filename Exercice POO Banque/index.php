3eb467495c2c047463619d4fa939ecbc5785d47e
Zicka67 <art.book@hotmail.fr>
2022-12-20 15:34:41 +0100

    Correction

--- Exercice POO Banque/index.php
+++ Exercice POO Banque/index.php
@@ -24,22 +24,13 @@
 
     // Instancier un titulaire de compte
     $titulaire = new Titulaire("Musk", "Elon", "28-06-1971", "Pretoria");
-
-
     // Instancier un compte
     $compte1 = new Compte("Compte courant", 5000000, "$", $titulaire);
     $compte2 = new Compte("Livret A", 1000000, "$", $titulaire);
 
     // echo $titulaire->afficherComptes();
 
-
     // var_dump($compte1);
-
-     //Calcule de l'âge
-    //  $dateNaissance = "28-06-1971";
-    //  $dateAujourdhui = date("Y-m-d");
-    //  $diff = date_diff(date_create($dateNaissance), date_create($dateAujourdhui));
-    //  echo $diff->format('%d' . '/' . '%m' . '/' . '%Y');
 
 
     ?>
