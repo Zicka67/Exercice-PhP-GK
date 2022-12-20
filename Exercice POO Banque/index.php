21a7012c132552a9248f4c03104a9857e584a5f3
Zicka67 <art.book@hotmail.fr>
2022-12-20 15:01:34 +0100

    Modifications et corrections dans index

--- Exercice POO Banque/index.php
+++ Exercice POO Banque/index.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Banque</title>
+    <title>Banque</title>
 
 </head>
 
 <body>
 
-<?php
+    <?php
 
-// require "Compte.php";
-// require "Titulaire.php";
-spl_autoload_register(function ($class_name) {
+    // require "Compte.php";
+    // require "Titulaire.php";
+    spl_autoload_register(function ($class_name) {
 
-    require_once $class_name . '.php';
+        require_once $class_name . '.php';
+    });
 
-});
-
-// Instancier un titulaire de compte
-$titulaire = new Titulaire("Musk", "Elon", 28-06-1971,"Pretoria");
-
-// Instancier un compte
-$compte1 = new Compte("Compte courant", 5000000, "Dollars", $titulaire);
-$compte2 = new Compte("Livret A", 1000000, "Dollars", $titulaire);
+    // Instancier un titulaire de compte
+    $titulaire = new Titulaire("Musk", "Elon", "28-06-1971", "Pretoria");
 
 
-echo "fqfqzzq";
+    // Instancier un compte
+    $compte1 = new Compte("Compte courant", 5000000, "$", $titulaire);
+    $compte2 = new Compte("Livret A", 1000000, "$", $titulaire);
 
-var_dump($compte1);
-
-
-
-echo $titulaire->afficherComptes;
-
-var_dump($compte1);
-var_dump($titulaire);
+    // echo $titulaire->afficherComptes();
 
 
-?>
+    // var_dump($compte1);
+
+     //Calcule de l'âge
+    //  $dateNaissance = "28-06-1971";
+    //  $dateAujourdhui = date("Y-m-d");
+    //  $diff = date_diff(date_create($dateNaissance), date_create($dateAujourdhui));
+    //  echo $diff->format('%d' . '/' . '%m' . '/' . '%Y');
+
+
+    ?>
 
 </body>
 
