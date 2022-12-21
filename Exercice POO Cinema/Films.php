02e7ae64523f43b9852b72d710998c3a092d4e35
Zicka67 <art.book@hotmail.fr>
2022-12-21 17:45:17 +0100

    Création de la class Film

---
+++ Exercice POO Cinema/Films.php
@@ -0,0 +1,10 @@
+<?php
+
+class Film
+{
+    private string $titre;
+    private string $dtSortie;
+    private int $duree;
+    private string $realisateur; //heritage de la class realisateur ?
+    private string $genre; //heritage de la class genre ?
+}   
