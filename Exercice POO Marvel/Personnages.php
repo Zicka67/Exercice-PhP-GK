712223524e8620fe0c17e1740afc22173ff953eb
Zicka67 <art.book@hotmail.fr>
2022-12-21 13:25:31 +0100

    Création de Personage

---
+++ Exercice POO Marvel/Personnages.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+class Personnage
+{
+    //On donne des caractéristiques a cet objet Personnage
+    public $vie = 100;
+    public $atk = 30;
+    
+    //Un exemple de function pour ce Personnage
+    public function crier()
+    {
+        echo "I'M BATMAN !";
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+?>
+
