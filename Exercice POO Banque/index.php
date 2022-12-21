231c7b54925de9dfda15c2d3193bc97bf6b1b0ae
Zicka67 <art.book@hotmail.fr>
2022-12-21 11:08:58 +0100

    Ajout de echo pour afficher les comptes

--- Exercice POO Banque/index.php
+++ Exercice POO Banque/index.php
@@ -3,48 +3,44 @@
 
 <head>
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Banque</title>
+<title>Banque</title>
 
 </head>
 
 <body>
 
-    <?php
+<?php
 
-    // require "Compte.php";
-    // require "Titulaire.php";
-    spl_autoload_register(function ($class_name) {
+// require "Compte.php";
+// require "Titulaire.php";
+spl_autoload_register(function ($class_name) {
+    
+    require_once $class_name . '.php';
+});
 
-        require_once $class_name . '.php';
-    });
+// Instancier un titulaire de compte
+$titulaire1 = new Titulaire("Musk", "Elon", "28-06-1971", "Pretoria");
+// Instancier un compte
+$compte1 = new Compte("Compte courant", 5000, "$", $titulaire1);
+$compte2 = new Compte("Livret A", 1000, "$", $titulaire1);
 
-    // Instancier un titulaire de compte
-    $titulaire = new Titulaire("Musk", "Elon", "28-06-1971", "Pretoria");
-    // Instancier un compte
-    $compte1 = new Compte("Compte courant", 5000000, "$", $titulaire);
-    $compte2 = new Compte("Livret A", 1000000, "$", $titulaire);
+$compte1->virement(0, $compte2);
+$compte1->crediter(0);
+$compte1->debiter(0);
 
-    $compte1->virement(1000000, $compte2);
-    $compte1->crediter(1000000);
-    $compte1->debiter(500000);
+$compte2->virement(0, $compte1);
+$compte1->crediter(0);
+$compte1->debiter(0);
 
-    $compte2->virement(0, $compte1);
-    $compte1->crediter(0);
-    $compte1->debiter(0);
-
-    // echo $compte1;
-    // echo $titulaire->afficherComptes();
-    // var_dump($titulaire);
-
-    echo $compte1 . "<br>";
-    echo $compte2 . "<br>";
+// echo $titulaire1->$nom;
+echo $titulaire1->afficherComptes();
 
 
-    ?>
+?>
 
 </body>
 
