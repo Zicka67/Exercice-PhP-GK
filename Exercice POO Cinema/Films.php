27eff21d8afe21782719bbdf6e46d530e7b83ae1
Zicka67 <art.book@hotmail.fr>
2022-12-22 10:58:24 +0100

    Création de __construct

--- Exercice POO Cinema/Films.php
+++ Exercice POO Cinema/Films.php
@@ -7,4 +7,22 @@ class Film
     private int $duree;
     private string $realisateur; //heritage de la class realisateur ?
     private string $genre; //heritage de la class genre ?
-}   
+    
+    
+    public function __construct(string $titre, string $dtsortie, int $duree, Realisateur $realisateur, Genre $genre)
+    {
+        $this->titre = $titre;
+        $this->dtSortie = $dtSortie;
+        $this->duree = $duree;
+        $this->realisateur = $realisateur;
+        $this->genre->addGenre($this);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+} 
