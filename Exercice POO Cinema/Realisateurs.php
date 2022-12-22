da2c514242d4a68f4c735ce881e7b934fffd8d51
Zicka67 <art.book@hotmail.fr>
2022-12-22 18:33:20 +0100

    Création du constuct + getter/setter

--- Exercice POO Cinema/Realisateurs.php
+++ Exercice POO Cinema/Realisateurs.php
@@ -5,5 +5,69 @@ class Realisateur
     private string $nom;
     private string $prenom;
     private string $sexe;
-    private DateTime $dtNaissance;
+    private string $dtNaissance;
+    
+    
+    public function __construct($nom, $prenom, $sexe, $dtNaissance)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->sexe = $sexe;
+        $this->dtNaissance = $dtNaissance;
+    }
+    
+    //******************** NOM ********************
+    
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    //******************** PRENOM ********************
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    //******************** SEXE ********************
+    public function getSexe(): string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    //******************** NDTNAISSANCE ********************
+    public function getDtNaissance()
+    {
+        return $this->dtNaissance;
+    }
+
+    public function setDtNaissance(DateTime $dtNaissance): self
+    {
+        $this->dtNaissance = $dtNaissance;
+
+        return $this;
+    }
+
+
 }   
