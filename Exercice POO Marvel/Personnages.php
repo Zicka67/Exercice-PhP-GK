899b9f6238a4c3394776c0c0b814ea6d211fd475
Zicka67 <art.book@hotmail.fr>
2022-12-22 10:55:56 +0100

    Création de la function __construct + function regenerer + function mort

--- Exercice POO Marvel/Personnages.php
+++ Exercice POO Marvel/Personnages.php
@@ -1,26 +1,62 @@
 <?php
 
-
-
 class Personnage
 {
     //On donne des caractéristiques a cet objet Personnage
     public $vie = 100;
-    public $atk = 30;
+    public $atk = 20;
+    public $nom;
     
-    //Un exemple de function pour ce Personnage
-    public function crier()
+    //Pour construire les paramètres du new Personnage
+    public function __construct($nom)
     {
-        echo "I'M BATMAN !";
+        $this->nom = $nom;
     }
     
-    
-    
-    
-    
-}
-
-
-
-?>
-
+    //Un exemple de function pour ce Personnage
+    // public function crier()
+    // {
+        //     echo "I'M BATMAN !";
+        // }
+        //Un exemple de function pour ce Personnage
+        public function regenerer($vie = null)
+        {
+            if(is_null($vie))
+            {
+                $this->vie = 100;
+            } else {
+                $this->vie = $this->vie + $vie;
+            }
+        }
+        
+        //Function pour voir si le personnage est mort
+        public function mort()
+        {
+            return $this->vie <= 0;
+        }
+        
+        //         ou
+        
+        // public function mort()
+        // {
+            //     if($this->vie <= 0) 
+            //     {
+                //         echo "$this->nom est mort ";
+                //     }
+                // }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }
+            
+            
+            
+            ?>
+            
