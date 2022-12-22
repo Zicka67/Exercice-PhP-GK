8724faa540d8417bf4f2e3fbd41c2d79264b6d78
Zicka67 <art.book@hotmail.fr>
2022-12-22 18:32:51 +0100

    Création du constuct + getter/setter

--- Exercice POO Cinema/Films.php
+++ Exercice POO Cinema/Films.php
@@ -1,28 +1,65 @@
 <?php
 
-class Film
-{
-    private string $titre;
-    private string $dtSortie;
-    private int $duree;
-    private string $realisateur; //heritage de la class realisateur ?
-    private string $genre; //heritage de la class genre ?
+class Film {
+    private $titre;
+    private $dtSortie;
+    private $duree;
+    private $realisateur;
     
-    
-    public function __construct(string $titre, string $dtsortie, int $duree, Realisateur $realisateur, Genre $genre)
-    {
+    public function __construct($titre, $dtSortie, $duree, Realisateur $realisateur) {
         $this->titre = $titre;
-        $this->dtSortie = $dtSortie;
+        $this->dateSortie = $dtSortie;
         $this->duree = $duree;
         $this->realisateur = $realisateur;
-        $this->genre->addGenre($this);
     }
     
+    //************** TITRE **************
+    public function getTitre()
+    {
+        return $this->titre;
+    }
     
+    public function setTitre($titre): self
+    {
+        $this->titre = $titre;
+        
+        return $this;
+    }
+    //************** DTSORTIE **************
+    public function getDtSortie()
+    {
+        return $this->dtSortie;
+    }
     
+    public function setDtSortie($dtSortie): self
+    {
+        $this->dtSortie = $dtSortie;
+        
+        return $this;
+    }
+    //************** DUREE **************
+    public function getDuree()
+    {
+        return $this->duree;
+    }
     
+    public function setDuree($duree): self
+    {
+        $this->duree = $duree;
+        
+        return $this;
+    }
+    //************** REALISATEUR **************
+    public function getRealisateur()
+    {
+        return $this->realisateur;
+    }
     
-    
-    
-    
-} 
+    public function setRealisateur($realisateur): self
+    {
+        $this->realisateur = $realisateur;
+        
+        return $this;
+    }
+
+}
