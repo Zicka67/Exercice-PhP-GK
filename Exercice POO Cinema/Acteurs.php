c991045089a8839541e58d643dcd2ba49b3004ec
Zicka67 <art.book@hotmail.fr>
2022-12-22 18:32:41 +0100

    Création du constuct + getter/setter

--- Exercice POO Cinema/Acteurs.php
+++ Exercice POO Cinema/Acteurs.php
@@ -5,6 +5,80 @@ class Acteur
     private string $nom;
     private string $prenom;
     private string $sexe;
-    private DateTime $dtNaissance;
-    private array $role;
+    private string $dtNaissance;
+    protected array $role;
+    
+    
+    public function __construct($nom, $prenom, $sexe, $dtNaissance, $role)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->sexe = $sexe;
+        $this->dtNaissance = $dtNaissance;
+        // $this->role = [];
+    }
+    //********************** NOM **********************
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+     //********************** PRENOM **********************
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+     //********************** SEXE **********************
+    public function getSexe(): string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+     //********************** DTNAISSANCE **********************
+    public function getDtNaissance()
+    {
+        return $this->dtNaissance;
+    }
+
+    public function setDtNaissance(DateTime $dtNaissance): self
+    {
+        $this->dtNaissance = $dtNaissance;
+
+        return $this;
+    }
+
+     //********************** ROLE **********************
+    public function getRole(): array
+    {
+        return $this->role;
+    }
+
+    public function setRole(array $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
 }   
+
