2ae7a13c6ca1a7f5d4d390d878e7c98572f5ae8a
Zicka67 <art.book@hotmail.fr>
2022-12-23 14:02:43 +0100

    Création de la class Personne qui va donner son héritage a Acteur et Réalisateur

---
+++ Exercice POO Cinema/Personne.php
@@ -0,0 +1,77 @@
+<?php
+
+class Personne 
+{
+    
+    protected string $nom;
+    protected string $prenom;
+    protected string $sexe;
+    protected string $dtNaissance;
+    
+    public function __construct($nom, $prenom, $sexe, $dtNaissance)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->sexe = $sexe;
+        $this->dtNaissance = $dtNaissance;
+    }
+    
+    //******************** NOM ********************
+    
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+    
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        
+        return $this;
+    }
+    
+    //******************** PRENOM ********************
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+    
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        
+        return $this;
+    }
+    
+    //******************** SEXE ********************
+    public function getSexe(): string
+    {
+        return $this->sexe;
+    }
+    
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+        
+        return $this;
+    }
+    
+    //******************** NDTNAISSANCE ********************
+    public function getDtNaissance()
+    {
+        return $this->dtNaissance;
+    }
+    
+    public function setDtNaissance(DateTime $dtNaissance): self
+    {
+        $this->dtNaissance = $dtNaissance;
+        
+        return $this;
+    }
+    
+    
+    
+    
+}
+
+
