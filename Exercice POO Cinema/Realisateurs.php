5a5c9dc5880af804901cf463311905a6f2caa2e9
Zicka67 <art.book@hotmail.fr>
2022-12-23 14:03:24 +0100

    Création de la function pour afficherFilmRea et du foreach

--- Exercice POO Cinema/Realisateurs.php
+++ Exercice POO Cinema/Realisateurs.php
@@ -1,73 +1,54 @@
 <?php
 
-class Realisateur
+
+class Realisateur extends Personne
 {
-    private string $nom;
-    private string $prenom;
-    private string $sexe;
-    private string $dtNaissance;
-    
+    private $tableauFilms = [];
     
     public function __construct($nom, $prenom, $sexe, $dtNaissance)
     {
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-        $this->sexe = $sexe;
-        $this->dtNaissance = $dtNaissance;
+        parent::__construct($nom, $prenom, $sexe, $dtNaissance);
     }
     
-    //******************** NOM ********************
+    public function getlistFilms(): array
+    {
+        return $this->listFilms;
+    }
     
-    public function getNom(): string
+    public function setlistFilms(array $listFilms): self
     {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
-
+        $this->listFilms = $listFilms;
+        
         return $this;
     }
-
-    //******************** PRENOM ********************
-    public function getPrenom(): string
-    {
-        return $this->prenom;
+    
+    public static function afficherFilmoRea($nomRea){ //Passer en paramètre $nomRea(qui va être ici notre variable pour 
+        //choisir le nom qu'on souhaite chercher et indiquer le nom souhaiter en paramètre dans l'index)
+        $films = [
+            $film = new Film("Star Wars 1", "13-10-1999", 136, $rea = new Realisateur("Lucas", "George", "Homme", "14-05-1944"), "Sf"),
+            $film = new Film("Batman Begins", "16-06-2005", 140, $rea = new Realisateur("Reeves", "Matt", "Homme", "27-04-1966"), "Action"),
+            $film = new Film("Monster", "14-04-2004", 111, $rea = new Realisateur("Jenkins", "Patty", "Femme", "24-07-1971"), "Thriller"),
+            $film = new Film("Le Smoking", "25-12-2002", 100, $rea = new Realisateur("Donovan", "Kevin", "Homme", "17-12-1971"), "Comedie"),
+            $film = new Film("Star Wars 2", "13-10-1999", 136, $rea = new Realisateur("Lucas", "George", "Homme", "14-05-1944"), "Sf"),
+            $film = new Film("Batman Returns", "16-06-2005", 140, $rea = new Realisateur("Reeves", "Matt", "Homme", "27-04-1966"), "Action"),
+            $film = new Film("Le Smoking 2", "25-12-2002", 100, $rea = new Realisateur("Donovan", "Kevin", "Homme", "17-12-1971"), "Comedie"),
+            $film = new Film("Star Wars 3", "13-10-1999", 136, $rea = new Realisateur("Lucas", "George", "Homme", "14-05-1944"), "Sf"),
+        ];        
+        
+        // ********************** Afficher la filmographie d'un Realisateur **********************
+        // variable de recherche
+        // $nomRea = "Lucas";
+        
+        // Parcourir les films et vérifier si le getNom du getRealisateur == au $nomRea mit en paramètre dans la function
+        foreach ($films as $film) {
+            if ($film->getRealisateur()->getNom() == $nomRea) {
+                //Affiche le titre
+                echo $film->getTitre() . "<br>";
+            }
+        }
+        
+        
     }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    //******************** SEXE ********************
-    public function getSexe(): string
-    {
-        return $this->sexe;
-    }
-
-    public function setSexe(string $sexe): self
-    {
-        $this->sexe = $sexe;
-
-        return $this;
-    }
-
-    //******************** NDTNAISSANCE ********************
-    public function getDtNaissance()
-    {
-        return $this->dtNaissance;
-    }
-
-    public function setDtNaissance(DateTime $dtNaissance): self
-    {
-        $this->dtNaissance = $dtNaissance;
-
-        return $this;
-    }
-
-
+    
+    
 }   
