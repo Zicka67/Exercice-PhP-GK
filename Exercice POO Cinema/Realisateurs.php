1eb4fdb2e6485c8d31e4e37d077c5d64ed8b3627
Zicka67 <art.book@hotmail.fr>
2023-01-03 16:37:39 +0100

    Ajout tableau vide tableauFilms et création de la function addFilms et AfficherFilmo

--- Exercice POO Cinema/Realisateurs.php
+++ Exercice POO Cinema/Realisateurs.php
@@ -5,9 +5,11 @@ class Realisateur extends Personne
 {
     private $tableauFilms = [];
     
+    
     public function __construct($nom, $prenom, $sexe, $dtNaissance)
     {
         parent::__construct($nom, $prenom, $sexe, $dtNaissance);
+        $this->tableauFilms = [];
     }
     
     public function getlistFilms(): array
@@ -22,18 +24,63 @@ class Realisateur extends Personne
         return $this;
     }
     
-    public static function afficherFilmoRea($films, $nomRea){ //Passer en paramètre $films et $nomRea. Pour que la function prenne $films de l'index
+    public function getTableauFilms()
+    {
+        return $this->tableauFilms;
+    }
+    
+    public function setTableauFilms($tableauFilms): self
+    {
+        $this->tableauFilms = $tableauFilms;
         
-        // Parcourir les films et vérifier si le getNom du getRealisateur == au $nomRea mit en paramètre dans la function
-        foreach ($films as $film) {
-            if ($film->getRealisateur()->getNom() == $nomRea) {
-                //Affiche le titre
-                echo $film->getTitre() . "<br>";
-            }
+        return $this;
+    }
+    
+    //************* ADDFILM ****************/
+    
+    public function addFilm($film){
+        $this->tableauFilms[] = $film;
+    }
+    
+    //************* AFFICHERFILMO ****************/
+    
+    public function afficherFilmo(){
+        foreach($this->tableauFilms as $film)
+        {
+            echo "Titre : " . $film->getTitre() . "<br>";
+            
         }
-        
-        
     }
     
     
-}   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // public static function afficherFilmoRea($films, $nomRea){ //Passer en paramètre $films et $nomRea. Pour que la function prenne $films de l'index
+        
+        //     // Parcourir les films et vérifier si le getNom du getRealisateur == au $nomRea mit en paramètre dans la function
+        //     foreach ($films as $film) {
+            //         if ($film->getRealisateur()->getNom() == $nomRea) {
+                //             //Affiche le titre
+                //             echo $film->getTitre() . "<br>";
+                //         }
+                //     }
+                
+                // }
+                
+            }
