356dd49e5ace0db30ae87a9c4f29fc62df4f017b
Zicka67 <art.book@hotmail.fr>
2023-01-03 16:40:00 +0100

    Modifications/Simplifications + Ajout de la function Addfilm et AfficherFilms

--- Exercice POO Cinema/Genres.php
+++ Exercice POO Cinema/Genres.php
@@ -1,37 +1,56 @@
 <?php
 
 class Genre {
-    private $genre;
     private $nom;
+    private $tabFilmsGenre = [];
     
-    public function __construct($nom) //faire hériter genre de la class film et rajouter le genre ?
+    public function __construct($nom)
     {
         $this->nom = $nom;
-    }
-    
-    public function getGenre()
-    {
-        return $this->genre;
-    }
-    
-    public function setGenre($genre): self
-    {
-        $this->genre = $genre;
-        
-        return $this;
+        $this->tabFilmsGenre = [];
     }
     
     public function getNom()
     {
         return $this->nom;
     }
-
+    
     public function setNom($nom): self
     {
         $this->nom = $nom;
-
+        
         return $this;
     }
+    
+    public function getTabFilmsGenre()
+    {
+        return $this->tabFilmsGenre;
+    }
+    
+    
+    public function setTabFilmsGenre($tabFilmsGenre): self
+    {
+        $this->tabFilmsGenre = $tabFilmsGenre;
+        
+        return $this;
+    }
+    
+    public function addFilm($film){
+        $this->tabFilmsGenre [] = $film;
+    }
+    
+    //************* AFFICHERFILMS ****************/
+    
+    public function afficherFilms(){
+        echo "<br>" . "Genre: " . $this->getNom() . "<br>";
+        foreach($this->tabFilmsGenre as $film) //On parcourt le tab tabFilmGenre qui appartient à l'objet en cours d'utilisation.
+        //$this fait référence à l'objet de la classe Genre, et tabFilmsGenre est un tableau de films appartenant à cet objet de la classe Genre.
+        {
+            echo "<br>" . "Nom du film : " . $film->getTitre() . "<br>";
+        }
+    }
+    
+    
 }  
 
 
