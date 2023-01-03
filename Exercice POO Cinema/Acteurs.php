e2f28fab24281a0aa1d252f6bcf2bc42d906cdc2
Zicka67 <art.book@hotmail.fr>
2023-01-03 09:43:17 +0100

    Ajout du role pour l'acteur et de la function afficherRoleParActeur + commentaires

--- Exercice POO Cinema/Acteurs.php
+++ Exercice POO Cinema/Acteurs.php
@@ -3,11 +3,14 @@
 class Acteur extends Personne
 {
     protected array $acteurs;
+    private string $role;
     
-    public function __construct($nom, $prenom, $sexe, $dtNaissance)
+    public function __construct($nom, $prenom, $sexe, $dtNaissance, $role)
     {
         parent::__construct($nom, $prenom, $sexe, $dtNaissance);
         $this->acteurs = [];
+        $this->role = $role;
+        
     }
     
     //****************** ACTEUR ******************
@@ -23,7 +26,20 @@ class Acteur extends Personne
         return $this;
     }
     
-
+    //****************** ROLE ******************
+    public function getRole()
+    {
+        return $this->role;
+    }
+    
+    public function setRole($role): self
+    {
+        $this->role = $role;
+        
+        return $this;
+    }
+    
+    
     // ************  ADDACTEUR **************
     public function addActeur($acteur){
         // Pour ajouter dans un tableau
@@ -42,6 +58,30 @@ class Acteur extends Personne
         }
     }
     
+    //créer un tab 
+    //parcourir le tab acteurs
+    //pour chaque acteur dans acteurs
+    //afficher son rôle
+    
+    
+    public static function afficherRoleParActeur($acteurs) {
+        $roleParActeur = []; //Création d'un tab vide
+        foreach ($acteurs as $acteur) { //Pour chaque $acteur dans $acteurs
+            $role = $acteur->getRole(); // On prend le role de l'acteur et il va être = a $role
+            if (!isset($roleParActeur[$role])) { // si le role dans le tab n'existe pas dans $roleParActeur 
+                $roleParActeur[$role] = []; // On l'ajoute dedans
+            }
+            $roleParActeur[$role][] = $acteur;//ajoute l'acteur au tab $roleParActeur associé a don role
+        }
+        foreach ($roleParActeur as $role => $acteurs) {
+            // Affichage du rôle
+            echo "<br>" . $role . "<br>";
+            // Affichage de la liste d'acteurs ayant ce rôle
+            foreach ($acteurs as $acteur) {
+                echo "- " . $acteur->getPrenom() . " " . $acteur->getNom() . "<br>";
+            }
+            echo "<br>";
+            
+        }
+    }
 }
-
-
