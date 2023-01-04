f783ce32f999c68abc28f022fe5a559fa7d181aa
Zicka67 <art.book@hotmail.fr>
2023-01-04 13:33:47 +0100

    Création du construct + getter/setter de la function casting

--- Exercice POO Cinema/Role.php
+++ Exercice POO Cinema/Role.php
@@ -3,9 +3,55 @@
 class Role
 {
     
+    private $nom;
+    private $casting;
     
+    public function __construct($role) {
+        $this->nom = $role;
+        $this->casting = [];
+        
+    }
     
+     //***************** ADDCASTING ****************
+     public function addCasting($newCasting){ 
+        $this->casting[] = $newCasting;
+    }
+
+    public function __toString()
+    {
+        return $this->nom;
+    }
     
+    public function listCasting()
+    {
+        echo $this->nom . " a été joué par : <BR>";
+        foreach ($this->casting as $casting) {
+            echo $casting->getActeur() . "<br>";
+        }
+    }
     
+    public function getNom()
+    {
+        return $this->nom;
+    }
     
+    public function setNom($nom): self
+    {
+        $this->nom = $nom;
+        
+        return $this;
+    }
+    
+    public function getCasting()
+    {
+        return $this->casting;
+    }
+    
+    public function setCasting($casting): self
+    {
+        $this->casting = $casting;
+        
+        return $this;
+    }
+
 }
