0366a21ff408258171c8f53ca7e417bbb9627c54
Zicka67 <art.book@hotmail.fr>
2023-01-04 13:36:26 +0100

    Ajout d'un tab vide " casting " et création de getDescription ansi que l'ajout de addCasting et infoCasting

--- Exercice POO Cinema/Films.php
+++ Exercice POO Cinema/Films.php
@@ -8,13 +8,17 @@ class Film
     protected $duree;
     protected $realisateur;
     protected $genre;
+    private $casting;
     
     public function __construct($titre, $dtSortie, $duree, $realisateur, $genre) {
         $this->titre = $titre;
         $this->dtSortie = $dtSortie;
         $this->duree = $duree;
         $this->realisateur = $realisateur;
-
+        $this->realisateur->addFilm($this); //test
+        $this->genre = $genre;
+        $this->casting = [];
+        
         $this->setGenre($genre);
         $this->realisateur->addFilm($this);
         
@@ -69,59 +73,50 @@ class Film
         return $this;
     }
     
-    //************** REALISATEUR **************
+    //************** GENRE **************
     public function getGenre()
     {
-        return $this->genre;
+        echo " Le film " . $this . " appartient au genre : " . $this->genre;
     }
     
-    
-    public function setGenre($genre)
-    {
+    public function setGenre($genre): self
+    { 
         $this->genre = $genre;
-        $genre->addFilm($this); // permet de lier chaque film a son genre.
-        //setGenre définit le genre d'un film. Et prend en para un objet de type "genre". Elle affecte cet objet a l'attribut "$genre" de l'objet "film".
-        //Elle appel la méthode addfilm de l'objet $genre et ajoute l'ojet Film en cours ($this) au tableau $tabFilmGenre de l'objet Genre
+        
+        return $this;
     }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //Créer la function qui a commme paramètre le tableau $films
-    // public static function afficherFilmsParGenre($films) {
-        
-    //     $filmsParGenre = [];// Création du tableau vide
-        
-    //     foreach ($films as $film) { //Pour chaque film dans le tableau $films
-    //         $nomGenre = $film->getGenre()->getNom(); //On prend le nom du genre dans $film et il va être = a $nomGenre
-    //         if (!isset($filmsParGenre[$nomGenre])) { //!isset vérifie si le genre dans le tab n'existe pas dans $filmsPArGenre 
-    //             $filmsParGenre[$nomGenre] = []; // 
-    //         }
-    //         //ajoute un film au tableau $filmParGenre dans la liste associé a $nomGenre(type de films). [] on ajoute les films dans un tableau vide.
-    //         $filmsParGenre[$nomGenre][] = $film;
-    //     }
-    //     foreach ($filmsParGenre as $nomGenre => $films) {
-    //         // Affichage du nom du genre
-    //         echo "<br>" . $nomGenre . "<br>";
-    //         // Affichage de la liste de films du genre
-    //         foreach ($films as $film) {
-    //             echo "- " . $film->getTitre() . "<br>";
-    //         }
-    //         echo "<br>";
-    //     }
-    // }
+    //***************** GETDESCRIPTION ****************
+    public function getDescription()
+    {
+        echo "<br>". "<br> *** Description de " . $this->getTitre() . " *** " .  ":<br>" . $this->titre . " est un film de genre " . $this->genre . " " . "réalisé par " . $this->realisateur . " et est sortie le " . $this->dtSortie  .  "<br> Durée : " . $this->duree . " minutes. <br>";
+        foreach ($this->casting as $casting) 
+        {
+            echo $casting->getActeur()->getPrenom() . " " . $casting->getActeur()->getNom() ." a joué ". $casting->getRole(). " dans ce film. <br>";
+        }
+    }
+    
+    //***************** ADDCASTING ****************
+    public function addCasting($Casting){ 
+        $this->casting[] = $Casting;
+    }
+    
+    //***************** GETCASTINGS ****************
+    public function infoCasting()
+    {
+        echo "<br>" . "*** Dans le film ". $this->getTitre() ." le casting est le suivant ***: <br>";
+        foreach ($this->casting as $casting) 
+        {
+            echo $casting->getActeur()->getPrenom() . " " . $casting->getActeur()->getNom() ." a joué ". $casting->getRole(). "<br>";
+        }
+    }
+    
+    public function __toString()
+    {
+        return $this->titre;
+    }
+    
+    
+    
+    
 }
