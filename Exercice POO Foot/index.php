c96386a1537817268e500348bda11f2210ecef85
Zicka67 <art.book@hotmail.fr>
2023-01-09 13:54:46 +0100

    Modification des clubs et carrières

--- Exercice POO Foot/index.php
+++ Exercice POO Foot/index.php
@@ -1,34 +1,40 @@
 <?php
 
-require "equipe.php";
+require "club.php";
 require "joueur.php";
-
+require "carriere.php";
+require "pays.php";
 
 $pays1 = new Pays("France");
 $pays2 = new Pays("Espagne");
 $pays3 = new Pays("Angleterre");
 $pays4 = new Pays("Italie");
 
-$club1 = new club("PSG", "1970");
-$club2 = new club("RCS", "1906");
-$club3 = new club("FC Barcelone", "1899");
-$club4 = new club("Real Madrid", "1902");
-$club5 = new club("Man United", "1878");
-$club6 = new club("Juventus", "1897");
+// var_dump($pays1);
 
-$joueur1 = new joueur("Messi", "Lionel", "1987");
-$joueur2 = new joueur("Mbappe", "Kilian", "1998");
-$joueur3 = new joueur("Junior", "Neymar", "1998");
-$joueur4 = new joueur("Ronaldo", "Cristiano", "1998");
-$joueur5 = new joueur("Gameiro", "Kevin", "1987");
-$joueur6 = new joueur("Benzema", "Karim", "1987");
+$club1 = new Club("PSG", "$pays1", "", "1970");
+$club2 = new Club("RCS", "$pays1", "","1906");
+$club3 = new Club("FC Barcelone", "$pays2", "","1899");
+$club4 = new Club("Real Madrid", "$pays2", "", "1902");
+$club5 = new Club("Man United", "$pays3", "", "1878");
+$club6 = new Club("Juventus", "$pays4", "", "1897");
 
-$carrier1 = new carriere($club1, $joueur2, "2017");
-$carrier2 = new carriere($club2, $joueur5, "2005");
-$carrier3 = new carriere($club3, $joueur3, "2013");
-$carrier4 = new carriere($club4, $joueur6, "2017");
-$carrier5 = new carriere($club5, $joueur4, "2003");
-$carrier6 = new carriere($club6, $joueur4, "2018");
-$carrier7 = new carriere($club3, $joueur1, "2001");
+$joueur1 = new Joueur("Messi", "Lionel", "1987");
+$joueur2 = new Joueur("Mbappe", "Kilian", "1998");
+$joueur3 = new Joueur("Junior", "Neymar", "1998");
+$joueur4 = new Joueur("Ronaldo", "Cristiano", "1998");
+$joueur5 = new Joueur("Gameiro", "Kevin", "1987");
+$joueur6 = new Joueur("Benzema", "Karim", "1987");
+
+$carrier1 = new Carriere($club1, $joueur2, "2017");
+$carrier2 = new Carriere($club2, $joueur5, "2005");
+$carrier3 = new Carriere($club3, $joueur3, "2013");
+$carrier4 = new Carriere($club4, $joueur6, "2017");
+$carrier5 = new Carriere($club5, $joueur4, "2003");
+$carrier6 = new Carriere($club6, $joueur4, "2018");
+$carrier7 = new Carriere($club3, $joueur1, "2001");
 $carrier8 = new carriere($club1, $joueur1, "2021");
-$carrier9 = new carriere($club1, $joueur3, "2017");
+$carrier9 = new Carriere($club1, $joueur3, "2017");
+
+var_dump($club1);
+
