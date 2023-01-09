944e8c10b1cb99881b7682a72105387eeac6259e
Zicka67 <art.book@hotmail.fr>
2023-01-09 11:40:31 +0100

    Creation de la class carriere

---
+++ Exercice POO Foot/carriere.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class Carriere
+{
+    private $equipeNom;
+    private $joueur;
+    private $dtArrivee;
+
+    public function __construct($equipeNom, $joueur, $dtArrivee)
+    {
+        $this->equipeNom = $equipeNom;
+        $this->joueur = $joueur;
+        $this->dtArrivee = $dtArrivee;
+    }
+
+    public function getEquipeNom()
+    {
+        return $this->equipeNom;
+    }
+
+    public function setEquipeNom($equipeNom): self
+    {
+        $this->equipeNom = $equipeNom;
+
+        return $this;
+    }
+
+    public function getJoueur()
+    {
+        return $this->joueur;
+    }
+
+    public function setJoueur($joueur): self
+    {
+        $this->joueur = $joueur;
+
+        return $this;
+    }
+
+    public function getDtArrivee()
+    {
+        return $this->dtStart;
+    }
+
+    public function setDtArrivee($DtArrivee): self
+    {
+        $this->DtArrivee = $DtArrivee;
+
+        return $this;
+    }
+
+}
