952448359277bf893081e4ba89a0b84df434f844
Zicka67 <art.book@hotmail.fr>
2023-01-09 16:58:29 +0100

    Ajout des get set

--- Exercice POO Foot/pays.php
+++ Exercice POO Foot/pays.php
@@ -4,15 +4,46 @@
 class Pays {
 
     private string $paysNom;
+    // private $equipe;
 
-public function __construct (string $paysNom)
+public function __construct (string $paysNom ) //$equipe)
 {
         $this->paysNom = $paysNom;
+        // $this->equipe = $equipe;
 }
+
+public function getPaysNom(): string
+{
+    return $this->paysNom;
+}
+
+public function setPaysNom(string $paysNom): self
+{
+    $this->paysNom = $paysNom;
+
+    return $this;
+}
+
+// public function getEquipe()
+// {
+//     return $this->equipe;
+// }
+
+// public function setEquipe($equipe): self
+// {
+//     $this->equipe = $equipe;
+
+//     return $this;
+// }
+
+
 
 public function __toString()
 {
     return $this->paysNom;
 }
+
+
+
 
 }
