9e44a33f608a84a40e76db6f2a9561c0521ac72b
Zicka67 <art.book@hotmail.fr>
2023-01-09 16:57:48 +0100

    Changement des noms de variables

--- Exercice POO Foot/index.php
+++ Exercice POO Foot/index.php
@@ -5,39 +5,42 @@ require "joueur.php";
 require "carriere.php";
 require "pays.php";
 
-$pays1 = new Pays("France");
-$pays2 = new Pays("Espagne");
-$pays3 = new Pays("Angleterre");
-$pays4 = new Pays("Italie");
+$france = new Pays("France");
+$espagne = new Pays("Espagne");
+$angleterre = new Pays("Angleterre");
+$italie = new Pays("Italie");
 
-$joueur1 = new Joueur("Messi", "Lionel", "1987");
-$joueur2 = new Joueur("Mbappe", "Kilian", "1998");
-$joueur3 = new Joueur("Junior", "Neymar", "1998");
-$joueur4 = new Joueur("Ronaldo", "Cristiano", "1998");
-$joueur5 = new Joueur("Gameiro", "Kevin", "1987");
-$joueur6 = new Joueur("Benzema", "Karim", "1987");
+$messi = new Joueur("Messi", "Lionel", "1987");
+$mbappe = new Joueur("Mbappe", "Kilian", "1998");
+$neymar = new Joueur("Junior", "Neymar", "1998");
+$ronaldo = new Joueur("Ronaldo", "Cristiano", "1998");
+$gameiro = new Joueur("Gameiro", "Kevin", "1987");
+$benzema = new Joueur("Benzema", "Karim", "1987");
 
-$club1 = new Club("PSG", "$pays1", "1970");
-$club2 = new Club("RCS", "$pays1", "1906");
-$club3 = new Club("FC Barcelone", "$pays2", "1899");
-$club4 = new Club("Real Madrid", "$pays2", "1902");
-$club5 = new Club("Man United", "$pays3", "1878");
-$club6 = new Club("Juventus", "$pays4", "1897");
+$psg = new Club("PSG", $france, "1970");
+$rcs = new Club("RCS", $france, "1906");
+$real = new Club("FC Barcelone", $espagne, "1899");
+$barca = new Club("Real Madrid", $espagne, "1902");
+$manU = new Club("Man United", $angleterre, "1878");
+$juv = new Club("Juventus", $italie, "1897");
 
 
-$carrier1 = new Carriere($club1, $joueur2, "2017");
-$carrier2 = new Carriere($club2, $joueur5, "2005");
-$carrier3 = new Carriere($club3, $joueur3, "2013");
-$carrier4 = new Carriere($club4, $joueur6, "2017");
-$carrier5 = new Carriere($club5, $joueur4, "2003");
-$carrier6 = new Carriere($club6, $joueur4, "2018");
-$carrier7 = new Carriere($club3, $joueur1, "2001");
-$carrier8 = new carriere($club1, $joueur1, "2021");
-$carrier9 = new Carriere($club1, $joueur3, "2017");
+$carriere1 = new Carriere($psg, $mbappe, "2017");
+$carriere2 = new Carriere($rcs, $gameiro, "2005");
+$carriere3 = new Carriere($real, $neymar, "2013");
+$carriere4 = new Carriere($barca, $benzema, "2017");
+$carriere5 = new Carriere($manU, $ronaldo, "2003");
+$carriere6 = new Carriere($juv, $ronaldo, "2018");
+$carriere7 = new Carriere($real, $messi, "2001");
+$carriere8 = new carriere($psg, $messi, "2021");
+$carriere9 = new Carriere($psg, $neymar, "2017");
 
-var_dump($club1);
+$ronaldo->afficherJoueur();
 echo "<br><br>";
-$joueur1->afficherJoueur();
+var_dump($ronaldo);
+echo "<br><br>";
+$messi->getClubs();
+print_r($carriere1);
 
 
 
