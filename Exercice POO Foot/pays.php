8f1f34b32490d69d651057c9207609e5b42ff9bf
Zicka67 <art.book@hotmail.fr>
2023-01-09 14:39:28 +0100

    Ajout de tostring

--- Exercice POO Foot/pays.php
+++ Exercice POO Foot/pays.php
@@ -3,12 +3,16 @@
 
 class Pays {
 
-    private $paysNom;
+    private string $paysNom;
 
-public function __construct ($paysNom)
+public function __construct (string $paysNom)
 {
         $this->paysNom = $paysNom;
 }
 
+public function __toString()
+{
+    return $this->paysNom;
+}
 
 }
