cea73de41d0c6d71c9f06a238fc54c3e70a7c394
Zicka67 <art.book@hotmail.fr>
2023-01-09 12:00:44 +0100

    Ajout de pays et joueur

--- Exercice POO Foot/club.php
+++ Exercice POO Foot/club.php
@@ -4,11 +4,15 @@
 class Club
 {
     private $clubNom;
+    private $pays;
+    private $joueur;
     private $dtCrea;
 
-    public function __construct($clubNom, $dtCrea)
+    public function __construct($clubNom, $pays, $joueur, $dtCrea)
     {
         $this->clubNom = $clubNom;
+        $this->pays = $pays;
+        $this->joueur = $joueur;
         $this->dtCrea = $dtCrea;
 
     }
@@ -21,6 +25,30 @@ class Club
     public function setClubNom($clubNom): self
     {
         $this->clubNom = $clubNom;
+
+        return $this;
+    }
+
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    public function setPays($pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getJoueur()
+    {
+        return $this->joueur;
+    }
+
+    public function setJoueur($joueur): self
+    {
+        $this->joueur = $joueur;
 
         return $this;
     }
@@ -44,5 +72,5 @@ class Club
 
 
 
-
+ 
 }
