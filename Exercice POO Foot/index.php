c03c48b118be48aedfb1dd1d24557d3fc776af72
Zicka67 <art.book@hotmail.fr>
2023-01-09 14:40:49 +0100

    Test de la function afficherJoueur

--- Exercice POO Foot/index.php
+++ Exercice POO Foot/index.php
@@ -10,21 +10,20 @@ $pays2 = new Pays("Espagne");
 $pays3 = new Pays("Angleterre");
 $pays4 = new Pays("Italie");
 
-// var_dump($pays1);
-
-$club1 = new Club("PSG", "$pays1", "", "1970");
-$club2 = new Club("RCS", "$pays1", "","1906");
-$club3 = new Club("FC Barcelone", "$pays2", "","1899");
-$club4 = new Club("Real Madrid", "$pays2", "", "1902");
-$club5 = new Club("Man United", "$pays3", "", "1878");
-$club6 = new Club("Juventus", "$pays4", "", "1897");
-
 $joueur1 = new Joueur("Messi", "Lionel", "1987");
 $joueur2 = new Joueur("Mbappe", "Kilian", "1998");
 $joueur3 = new Joueur("Junior", "Neymar", "1998");
 $joueur4 = new Joueur("Ronaldo", "Cristiano", "1998");
 $joueur5 = new Joueur("Gameiro", "Kevin", "1987");
 $joueur6 = new Joueur("Benzema", "Karim", "1987");
+
+$club1 = new Club("PSG", "$pays1", "$joueur1", "1970");
+$club2 = new Club("RCS", "$pays1", "$joueur5", "1906");
+$club3 = new Club("FC Barcelone", "$pays2", "", "1899");
+$club4 = new Club("Real Madrid", "$pays2", "", "1902");
+$club5 = new Club("Man United", "$pays3", "$joueur4", "1878");
+$club6 = new Club("Juventus", "$pays4", "$joueur4", "1897");
+
 
 $carrier1 = new Carriere($club1, $joueur2, "2017");
 $carrier2 = new Carriere($club2, $joueur5, "2005");
@@ -37,4 +36,8 @@ $carrier8 = new carriere($club1, $joueur1, "2021");
 $carrier9 = new Carriere($club1, $joueur3, "2017");
 
 var_dump($club1);
+echo "<br><br>";
+$joueur2->afficherJoueur();
+
+
 
