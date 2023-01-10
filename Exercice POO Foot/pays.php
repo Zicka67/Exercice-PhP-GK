de38935670f636f273593f9e184dc7c117c7b34d
Zicka67 <art.book@hotmail.fr>
2023-01-10 13:02:55 +0100

    Ajout de la function addEquipe et de afficherEquipe mineurs changements

--- Exercice POO Foot/pays.php
+++ Exercice POO Foot/pays.php
@@ -2,48 +2,67 @@
 
 
 class Pays {
-
+    
     private string $paysNom;
-    // private $equipe;
-
-public function __construct (string $paysNom ) //$equipe)
-{
+    private $listEquipe;
+    
+    public function __construct (string $paysNom ) //$equipe)
+    {
         $this->paysNom = $paysNom;
-        // $this->equipe = $equipe;
-}
-
-public function getPaysNom(): string
-{
-    return $this->paysNom;
-}
-
-public function setPaysNom(string $paysNom): self
-{
-    $this->paysNom = $paysNom;
-
-    return $this;
-}
-
-// public function getEquipe()
-// {
-//     return $this->equipe;
-// }
-
-// public function setEquipe($equipe): self
-// {
-//     $this->equipe = $equipe;
-
-//     return $this;
-// }
-
-
-
-public function __toString()
-{
-    return $this->paysNom;
-}
-
-
-
-
+        $this->listEquipe = [];
+    }
+    
+    public function getPaysNom(): string
+    {
+        return $this->paysNom;
+    }
+    
+    public function setPaysNom(string $paysNom): self
+    {
+        $this->paysNom = $paysNom;
+        
+        return $this;
+    }
+    
+    public function getListEquipe()
+    {
+        return $this->listEquipe;
+    }
+    
+    public function setListEquipe($listEquipe): self
+    {
+        $this->listEquipe = $listEquipe;
+        
+        return $this;
+    }
+    
+    // *** POUR AJOUTER ET AFFICHER LES AJOUTS ***
+    public function addEquipe($equipe)
+    {
+        $this->listEquipe[] = $equipe;
+    }
+    
+    
+    public function afficherEquipe()
+    {
+        $result = "Equipes du pays $this :" . "<br>";
+        foreach($this->listEquipe as $equipe){
+            $result .= $equipe . "<br>";
+        }
+        return $result;
+    }
+    // ***                                    ***
+    
+    
+    public function __toString()
+    {
+        return $this->getPaysNom();
+    }
+    
+    
+    
+    
+    
+    
+    
 }
