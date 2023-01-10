a6b27ff03890b5ed992055dee68e0eca33a9c093
Zicka67 <art.book@hotmail.fr>
2023-01-10 13:54:31 +0100

    Modifications mineurs

--- Exercice POO Foot/index.php
+++ Exercice POO Foot/index.php
@@ -6,22 +6,23 @@ require "carriere.php";
 require "pays.php";
 require "nationalite.php";
 
-$nationalite = new Nationalite("Fr");
-$nationalite = new Nationalite("Esp");
-$nationalite = new Nationalite("En");
-$nationalite = new Nationalite("It");
+$fr = new Nationalite("Fr");
+$arg = new Nationalite("Ar");
+$br = new Nationalite("Br");
+$port = new Nationalite("Pt");
+
 
 $france = new Pays("France");
 $espagne = new Pays("Espagne");
 $angleterre = new Pays("Angleterre");
 $italie = new Pays("Italie");
 
-$messi = new Joueur("Messi", "Lionel", "1987");
-$mbappe = new Joueur("Mbappe", "Kilian", "1998");
-$neymar = new Joueur("Junior", "Neymar", "1998");
-$ronaldo = new Joueur("Ronaldo", "Cristiano", "1998");
-$gameiro = new Joueur("Gameiro", "Kevin", "1987");
-$benzema = new Joueur("Benzema", "Karim", "1987");
+$messi = new Joueur("Messi", "Lionel", "1987", "$fr", "$psg");
+$mbappe = new Joueur("Mbappe", "Kilian", "1998", "$fr", "$psg");
+$neymar = new Joueur("Junior", "Neymar", "1998", "$br", "$psg");
+$ronaldo = new Joueur("Ronaldo", "Cristiano", "1998", "$port", "$manU");
+$gameiro = new Joueur("Gameiro", "Kevin", "1987", "$fr", "$rcs");
+$benzema = new Joueur("Benzema", "Karim", "1987", "$fr", "$real");
 
 $psg = new Club("PSG", $france, "1970");
 $rcs = new Club("RCS", $france, "1906");
@@ -41,11 +42,11 @@ $carriere7 = new Carriere($real, $messi, "2001");
 $carriere8 = new carriere($psg, $messi, "2021");
 $carriere9 = new Carriere($psg, $neymar, "2017");
 
-$ronaldo->afficherJoueur();
+
 echo "<br><br>";
 var_dump($ronaldo);
 echo "<br><br>";
-$messi->getClubs();
+
 print_r($carriere1);
 
 
